b3f44468a0d0fc1f6cd930270db4db1e226c014d
JonathanRojas <jonathan.rojas.roco@gmail.com>
2018-11-11 17:55:37 -0300

    Plantilla Correo

--- Agenda/default.aspx.vb
+++ Agenda/default.aspx.vb
@@ -3,6 +3,7 @@ Public Class _default
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Response.Redirect("~/agenda")
     End Sub
 
 End Class
--- Agenda/doPost.asmx.vb
+++ Agenda/doPost.asmx.vb
@@ -38,8 +38,8 @@ Public Class doPost1
         Dim vEspecialista As Especialista = js.Deserialize(Context.Request.Form("especialista"), GetType(Especialista))
         Dim vResult As New httpResult
 
-        'If vEspecialista.registrarEspecialista() Then
-        If vEspecialista.enviarCorreo("jonathan.rojas.roco@gmail.com", "jrojas", "jr.1569") Then
+        If vEspecialista.registrarEspecialista() Then
+            '  If vEspecialista.enviarCorreo("jonathan.rojas.roco@gmail.com", "jrojas", "jr.1569") Then
             vResult.result = True
         Else
             vResult.result = False
