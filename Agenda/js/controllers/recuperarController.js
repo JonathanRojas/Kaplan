676412f83fc9f6f240d0c16fd9ad64858bf2cd9c
JonathanRojas <jonathan.rojas.roco@gmail.com>
2018-11-14 23:04:19 -0300

    Recuperación de contraseña

--- Agenda/js/controllers/recuperarController.js
+++ Agenda/js/controllers/recuperarController.js
@@ -1,5 +1,17 @@
 ﻿app.controller("recuperarController", ['$scope', 'Notification', '$location', 'LoginService',
 function ($scope, Notification, $location, LoginService) {
-    $scope.hola = "";
+    
+    $scope.formSubmit = function () {
+        $("#btnGuardar").button('loading');
+        LoginService.getCorreo($scope.Email).then(function (result) {
+                msg = { title: 'Email enviado' };
+                Notification.success(msg);
+                $("#btnGuardar").button('reset');
+                $location.path('/');
+            }, function (reason) {
+                Notification.error('Error al enviar correo');
+                $("#btnGuardar").button('reset');
+            });
+    };
 }]);
 
