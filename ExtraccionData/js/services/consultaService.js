41288fd20dd8a5bbd3ad15d9c75da734093a1e25
CANDIA666 <CANDIA666@CANDIA666>
2018-12-23 22:43:45 -0300

    Avance Sistema exportar Data

---
+++ ExtraccionData/js/services/consultaService.js
@@ -0,0 +1,25 @@
+﻿app.factory('consultaService', ['$http', '$q', function ($http, $q) {
+    var Serv = [];
+
+    Serv.getConsulta = function (rut) {
+        var deferred = $q.defer();
+        $http({
+            method: "GET",
+            async: true,
+            url: 'doGet.asmx/getConsulta'
+        }).then(onSuccess, onFailure);
+        function onSuccess(response) {
+            if (response.data.result)
+            { deferred.resolve(response.data); }
+            else
+            { deferred.reject(response.data) }
+        }
+        function onFailure(response) {
+            deferred.reject(response);
+        };
+        return deferred.promise;
+
+    };
+
+    return Serv;
+}]);
