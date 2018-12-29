941133ddff33e6413cc053e6e2f13ca5fb3c235b
JonathanRojas <jonathan.rojas.roco@gmail.com>
2018-12-29 18:18:20 -0300

    Avance carga excel

--- Kaplan/models/paciente/Archivo.vb
+++ Kaplan/models/paciente/Archivo.vb
@@ -2,8 +2,6 @@
 Imports System.Globalization
 Imports System.Data.OleDb
 Imports System.Data.SqlClient
-Imports Microsoft.Office.Interop
-
 Namespace Clases
     Public Class Archivo
         Public Property Id As Integer
@@ -49,34 +47,144 @@ Namespace Clases
         'End Function
         Public Function registrarArchivo(ruta As String, contenido As Byte()) As Boolean
             Try
-                'Dim olecon As OleDbConnection
-                'Dim olecomm As OleDbCommand
-                'Dim oleadpt As OleDbDataAdapter
-                'Dim ds As DataSet
-                'Dim connstring As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + ruta + ";Extended Properties=""Excel 8.0;HDR=NO;"""
-                'olecon = New OleDbConnection
-                'olecon.ConnectionString = connstring
-                'olecomm = New OleDbCommand
-                'olecomm.CommandText = "Select * from [Ergo$A60:Z]"
-                'olecomm.Connection = olecon
-                'oleadpt = New OleDbDataAdapter(olecomm)
-                'ds = New DataSet
+                Dim olecon As OleDbConnection
+                Dim olecomm As OleDbCommand
+                Dim oleadpt As OleDbDataAdapter
+                Dim ds As DataSet
+                Dim connstring As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + ruta + ";Extended Properties=""Excel 8.0;HDR=NO;"""
+                olecon = New OleDbConnection
+                olecon.ConnectionString = connstring
+                olecomm = New OleDbCommand
+                'olecomm.CommandText = "Select * from [Ergo$A8:L8] "
+                olecomm.CommandText = "Select * from [Ergo$A60:Z] "
+                olecomm.Connection = olecon
+                oleadpt = New OleDbDataAdapter(olecomm)
+                ds = New DataSet
 
-                'olecon.Open()
-                'oleadpt.Fill(ds, "Fijo")
-                'registrarArchivo = True
-                'Dim oApp As New Excel.Application
-                'Dim oWBa As Excel.Workbook = oApp.Workbooks.Open("c:\Test.XLS")
-                'Dim oWS As Excel.Worksheet = DirectCast(oWBa.Worksheets(1),
-                'Excel.Worksheet)
-                'oApp.Visible = False
+                olecon.Open()
+                oleadpt.Fill(ds, "Fijo")
+                Dim jsonDemo As String = GetJson(ds.Tables("fijo"))
 
-                'Dim oRng As Excel.Range
-                'oRng = oWS.Range("D6")
-                'MsgBox(oRng.Value)
+                cargarArchivo(jsonDemo)
+
+                registrarArchivo = True
             Catch exc As Exception
                 registrarArchivo = False
             End Try
+        End Function
+        Public Function cargarArchivo(archivo As String) As Boolean
+            Dim conn As OleDbConnection = New OleDbConnection(ConfigurationManager.ConnectionStrings("ConexionKaplan").ConnectionString)
+            Dim cmd As OleDbCommand = New OleDbCommand("CargarArchivo", conn)
+            cmd.CommandType = CommandType.StoredProcedure
+
+            'Dim inId As OleDbParameter = cmd.Parameters.Add("@id_ficha", OleDbType.Decimal, Nothing)
+            'inId.Direction = ParameterDirection.Input
+            'inId.Value = Me.Id
+
+            Dim indiagnostico As OleDbParameter = cmd.Parameters.Add("@archivo", OleDbType.VarChar, -1)
+            indiagnostico.Direction = ParameterDirection.Input
+            indiagnostico.Value = archivo
+
+            Dim outError As OleDbParameter = cmd.Parameters.Add("@outError", OleDbType.Integer)
+            outError.Direction = ParameterDirection.Output
+
+            conn.Open()
+            cmd.ExecuteReader()
+            conn.Close()
+
+            Dim idkine = CInt(cmd.Parameters("@outIdKine").Value)
+
+            Return CInt(cmd.Parameters("@outError").Value)
+        End Function
+        Public Function GetJson(ByVal dt As DataTable) As String
+            Dim serializer As New System.Web.Script.Serialization.JavaScriptSerializer()
+            Dim rows As New List(Of Dictionary(Of String, Object))()
+            Dim row As Dictionary(Of String, Object) = Nothing
+            For Each dr As DataRow In dt.Rows
+                row = New Dictionary(Of String, Object)()
+                For Each dc As DataColumn In dt.Columns
+                    If dc.ColumnName.Trim() = "F1" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F2" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F3" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F4" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F5" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F6" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F7" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F8" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F9" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F10" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F11" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F12" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F13" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F14" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F15" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F16" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F17" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F18" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F19" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F20" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F21" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F22" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F23" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F24" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F25" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                    If dc.ColumnName.Trim() = "F26" Then
+                        row.Add(dc.ColumnName.Trim(), dr(dc))
+                    End If
+                Next
+                rows.Add(row)
+            Next
+            Return serializer.Serialize(rows)
         End Function
         'Public Function EliminarExamen(Id As Integer) As Boolean
         '    Try
